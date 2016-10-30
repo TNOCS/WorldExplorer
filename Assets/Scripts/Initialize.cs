8619c121b53cca6c04b5dd8a952f18849100630c
Erik Vullings <erik.vullings@gmail.com>
2016-10-30 21:23:38 +0100

    Initialize.cs: fix warnings, add checks
    - Removed unused properties (_latitude, etc)
    - Instead of using x.transform.parent = use x.transform.SetParent
    - Added range check (inside mapScale length)
    - Added world == null check in Update
    - Uncommented SymbolFactory (gave errors)

--- Assets/Scripts/Initialize.cs
+++ Assets/Scripts/Initialize.cs
@@ -13,10 +13,10 @@ public class Initialize : MonoBehaviour
     private AppState appState;
     private GameObject table;
     float[] mapScales = new float[] { 0.004f, 0.002f, 0.00143f, 0.00111f, 0.00091f, 0.00077f, 0.000666f };
-    private float _latitude;
-    private float _longitude;
-    private int _range;
-    private int _zoom;
+    //private float _latitude;
+    //private float _longitude;
+    //private int _range;
+    //private int _zoom;
     //private int _TitleSize = 100;
     public string json = "{   \"type\": \"FeatureCollection\",   \"features\": [     {       \"geometry\": {         \"type\": \"Point\",         \"coordinates\": [           5.070362091064453,           53.295336751980656         ]       },       \"type\": \"Feature\",       \"properties\": {         \"kind\": \"forest\",         \"area\": 35879,         \"source\": \"openstreetmap.org\",         \"min_zoom\": 14,         \"tier\": 2,         \"id\": 119757239, 		 \"symbol\": \"liaise.png\"       }     },     {       \"geometry\": {         \"type\": \"Point\",         \"coordinates\": [           5.072250366210937,           53.29523415150025         ]       },       \"type\": \"Feature\",       \"properties\": {         \"kind\": \"forest\",         \"area\": 1651,         \"source\": \"openstreetmap.org\",         \"min_zoom\": 14,         \"tier\": 2,         \"id\": 119757777, 		 \"symbol\": \"counterattack_fire.png\"       }     },     {       \"geometry\": {         \"type\": \"Point\",         \"coordinates\": [           5.066671371459961,           53.29469549493482         ]       },       \"type\": \"Feature\",       \"properties\": {         \"marker-color\": \"#7e7e7e\",         \"marker-size\": \"medium\",         \"marker-symbol\": \"circle-stroked\",         \"kind\": \"app-622\",         \"area\": 18729,         \"source\": \"openstreetmap.org\",         \"min_zoom\": 14,         \"tier\": 2,         \"id\": 119758146,         \"symbol\": \"warrant_served.png\"       }     },     {       \"geometry\": {         \"type\": \"Point\",         \"coordinates\": [           5.068731307983398,           53.29497764922103         ]       },       \"type\": \"Feature\",       \"properties\": {         \"kind\": \"bus_stop\",         \"name\": \"Eureka\",         \"source\": \"openstreetmap.org\",         \"min_zoom\": 17,         \"operator\": \"TCR\",         \"id\": 2833355779, 		 \"symbol\": \"activity.png\"       }     }   ] }";
     void includeAnchorMovingScript()
@@ -46,15 +46,10 @@ public class Initialize : MonoBehaviour
     void Start()
     {
         appState = AppState.Instance;
-
-        // get config
         appState.LoadConfig();
         
         AddTerrain();
-
-
         // includeAnchorMovingScript();
-
     }
 
     protected void AddTerrain()
@@ -69,14 +64,16 @@ public class Initialize : MonoBehaviour
         table = GameObject.CreatePrimitive(PrimitiveType.Cube);
         table.transform.position = new Vector3(0f, 0f, 3f);
         table.transform.localScale = new Vector3(iv.TableSize, iv.TableHeight, iv.TableSize);
-        table.transform.parent = terrain.transform;
+        table.transform.SetParent(terrain.transform, false);
 
 
         var map = new GameObject("Map");
-        map.transform.parent = table.transform;
+        map.transform.SetParent(table.transform);
         map.transform.localPosition = new Vector3(0f, 0.5f, 0f);
-        
-        var mapScale = mapScales[iv.Range-1];
+
+        var i = iv.Range;
+        if (i > mapScales.Length) i = mapScales.Length;
+        var mapScale = mapScales[i - 1];
         map.transform.localScale = new Vector3(mapScale, mapScale, mapScale);
 
         #endregion       
@@ -84,7 +81,7 @@ public class Initialize : MonoBehaviour
         #region init map
 
         world = new GameObject("World");
-        world.transform.parent = map.transform;
+        world.transform.SetParent(map.transform, false);
 
         // init map
         var tm = world.AddComponent<TileManager>();
@@ -100,81 +97,86 @@ public class Initialize : MonoBehaviour
         #region UI
 
         var ui = new GameObject("UI"); // Placeholder (root element in UI tree)
-        ui.transform.parent = world.transform;
+        ui.transform.SetParent(world.transform, false);
         var place = new GameObject("PlaceContainer");
         AddRectTransformToGameObject(place);
-        place.transform.parent = ui.transform;
+        place.transform.SetParent(ui.transform, false);
 
         var poi = new GameObject("PoiContainer");
         AddRectTransformToGameObject(poi);
-        poi.transform.parent = ui.transform;
+        poi.transform.SetParent(ui.transform, false);
 
         #endregion
 
         #region FACTORIES
 
         var factories = new GameObject("Factories");
-        factories.transform.parent = world.transform;
+        factories.transform.SetParent(world.transform, false);
 
         var buildings = new GameObject("BuildingFactory");
-        buildings.transform.parent = factories.transform;
+        buildings.transform.SetParent(factories.transform, false);
         var buildingFactory = buildings.AddComponent<BuildingFactory>();
 
         var flatBuildings = new GameObject("FlatBuildingFactory");
-        flatBuildings.transform.parent = factories.transform;
+        flatBuildings.transform.SetParent(factories.transform, false);
         var flatBuildingFactory = flatBuildings.AddComponent<FlatBuildingFactory>();
 
         var roads = new GameObject("RoadFactory");
-        roads.transform.parent = factories.transform;
+        roads.transform.SetParent(factories.transform, false);
         var roadFactory = roads.AddComponent<RoadFactory>();
 
         var water = new GameObject("WaterFactory");
-        water.transform.parent = factories.transform;
+        water.transform.SetParent(factories.transform, false);
         var waterFactory = water.AddComponent<WaterFactory>();
 
         var boundary = new GameObject("BoundaryFactory");
-        boundary.transform.parent = factories.transform;
+        boundary.transform.SetParent(factories.transform, false);
         var boundaryFactory = boundary.AddComponent<BoundaryFactory>();
 
         var landuse = new GameObject("LanduseFactory");
-        landuse.transform.parent = factories.transform;
+        landuse.transform.SetParent(factories.transform, false);
         var landuseFactory = landuse.AddComponent<LanduseFactory>();
 
         var places = new GameObject("PlacesFactory");
-        places.transform.parent = factories.transform;
+        places.transform.SetParent(factories.transform, false);
         var placesFactory = places.AddComponent<PlacesFactory>();
 
         var pois = new GameObject("PoiFactory");
-        pois.transform.parent = factories.transform;
+        pois.transform.SetParent(factories.transform, false);
         var poisFactory = pois.AddComponent<PoiFactory>();
 
 
         //var _symbolicInitHandler = world.AddComponent<SymbolFactory>();
         //_symbolicInitHandler.geojson = json;
         //_symbolicInitHandler.AddSymbols();
-        var _symbolicInitHandler = new GameObject("SymbolFactory");
-        _symbolicInitHandler.transform.localScale = terrain.transform.localScale;
-        //_symbolicInitHandler.transform.parent = factories.transform;
-        var symbolFactory = _symbolicInitHandler.AddComponent<SymbolFactory>();
-        symbolFactory.geojson = json;
-        symbolFactory.zoom = iv.Zoom;
-        symbolFactory.Latitude = iv.Lat;
-        symbolFactory.Longitude = iv.Lon;
-        symbolFactory.TileSize = iv.TileSize;
-        symbolFactory.Range = iv.Range;
-        symbolFactory.AddSymbols();
+        //var _symbolicInitHandler = new GameObject("SymbolFactory");
+        //_symbolicInitHandler.transform.localScale = terrain.transform.localScale;
+        ////_symbolicInitHandler.transform.SetParent(factories.transform, false);
+        //var symbolFactory = _symbolicInitHandler.AddComponent<SymbolFactory>();
+        //symbolFactory.geojson = json;
+        //symbolFactory.zoom = iv.Zoom;
+        //symbolFactory.Latitude = iv.Lat;
+        //symbolFactory.Longitude = iv.Lon;
+        //symbolFactory.TileSize = iv.TileSize;
+        //symbolFactory.Range = iv.Range;
+        //symbolFactory.AddSymbols();
 
         #endregion
 
         #region TILE PLUGINS
 
         var tilePlugins = new GameObject("TilePlugins");
-        tilePlugins.transform.parent = world.transform;
+        tilePlugins.transform.SetParent(world.transform, false);
 
         var mapImage = new GameObject("MapImage");
-        mapImage.transform.parent = tilePlugins.transform;
+        mapImage.transform.SetParent(tilePlugins.transform, false);
         var mapImagePlugin = mapImage.AddComponent<MapImagePlugin>();
         mapImagePlugin.TileService = MapImagePlugin.TileServices.Default;
+
+        var tileLayer = new GameObject("TileLayer");
+        tileLayer.transform.SetParent(tilePlugins.transform, false);
+        var tileLayerPlugin = tileLayer.AddComponent<TileLayerPlugin>();
+        tileLayerPlugin.tileLayers = appState.Config.Layers;
 
         #endregion
 
@@ -192,7 +194,10 @@ public class Initialize : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //world.transform.localScale = new Vector3(0.001F, 0.001F, 0.001F);
-        world.transform.localPosition = new Vector3(0,0,0);
+        if (world != null)
+        {
+            //world.transform.localScale = new Vector3(0.001F, 0.001F, 0.001F);
+            world.transform.localPosition = new Vector3(0, 0, 0);
+        }
     }
 }
